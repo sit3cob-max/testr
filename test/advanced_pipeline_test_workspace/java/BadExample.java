8ac3675645c1541a913a2e31c7c05d284a135434
sit3cob-max <sit3cob@bosch.com>
2026-08-20 11:36:13 +0530

    Delete test directory

--- test/advanced_pipeline_test_workspace/java/BadExample.java
+++
@@ -1,7 +0,0 @@
-public class BadExample {
-    private static final String password = "Admin123456";
-
-    public static void main(String[] args) {
-        System.out.println("debug java output");
-    }
-}
--- test/advanced_pipeline_test_workspace/run_local_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python3 automatic.py --workspace . --coverage-file coverage.xml --junit-pattern "reports/TEST-*.xml"
-echo "Open pipeline_improver_report.html in your browser"
